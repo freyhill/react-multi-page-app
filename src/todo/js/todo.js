53da08d12dc4dc4a3f31ed18a59c3f083599a6d4
leinov <1425795233@qq.com>
2018-12-07 18:35:14 +0800

    todo

--- src/todo/js/todo.js
+++ src/todo/js/todo.js
@@ -5,7 +5,7 @@ class Todo extends React.Component {
 		return (
 			<li className="todo-item flex v-center">
 				<span className={`${this.props.todoItem.completed ? "todo-checked" :""} todo-checkbox `} onClick={this.props.onClick}></span>	
-				<span className={`${this.props.todoItem.completed ? "todo-true" :"todo-false"} flex1 toto-text` }>{this.props.todoItem.text}</span>
+				<span className={`${this.props.todoItem.completed ? "todo-true" :"todo-false"} flex1 toto-text` } onClick={this.props.onClick}>{this.props.todoItem.text}</span>
 				<span className={`${this.props.todoItem.completed ? "todo-true" :"todo-false"}  toto-text v-center` } onClick={this.props.onDeleteClick}><i className="fa fa-trash-alt delete-todo"></i></span>
 			</li>
 		);
